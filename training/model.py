6ab2276369f2b79b091422b3c2568263afbe88fa
Rojer <saifullah52981@gmail.com>
2026-01-15 15:38:25 +0500

    Updated Model With Forward Pass

--- training/model.py
+++ training/model.py
@@ -12,4 +12,18 @@ class TextCNN(nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(num_filters * len(kernel_sizes), 1)
 
+    def forward(self, x):
+        emb = self.embedding(x)          # (B, L, E)
+        emb = emb.transpose(1, 2)        # (B, E, L)
+
+        pooled = []
+        for conv in self.convs:
+            h = F.relu(conv(emb))        # (B, F, L')
+            h = F.max_pool1d(h, h.size(2)).squeeze(2)  # (B, F)
+            pooled.append(h)
+
+        feats = torch.cat(pooled, dim=1)
+        feats = self.dropout(feats)
+        logits = self.fc(feats).squeeze(1)  # (B,)
+        return logits
     
